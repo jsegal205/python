9e2e14e65bd86dc71ad7685f7faf9488757dac27
Jim Segal <hello@jimsegal.com>
2020-11-17 20:42:03 -0600

    pass dot files to hidden directory

--- folder-sorter/run.py
+++ folder-sorter/run.py
@@ -15,8 +15,14 @@ files = [f for f in listdir(inpath) if isfile(join(inpath, f))]
 
 print("Found {} files to be sorted".format(len(files)))
 for filename in files:
-    ext = filename.split(".")[1]
+    fname_split = filename.split(".")
+    ext = fname_split[len(fname_split)-1]
+
     targetpath = join(inpath, ext)
+    if fname_split[0] == "":
+        # dot files are hidden by default on osx, so this will be a visible
+        # hidden folder with no visibile items in it. :D
+        targetpath = join(inpath, "hidden")
 
     if grouped.lower() == "y":
         for grouping_folder, grouping_exts in groupings.items():
