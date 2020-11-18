67b064b9729eeafc36605c0c3e27fb671355b4fa
Jim Segal <hello@jimsegal.com>
2020-11-17 20:31:12 -0600

    add grouping logic

--- folder-sorter/README.md
+++ folder-sorter/README.md
@@ -1,6 +1,8 @@
 # Folder sorter
 
-This will sort all the files in one folder to subfolders by extention
+This will sort all the files in one folder to subfolders by extention.
+
+This can be helpful to clean up the always cluttered downloads folder
 
 ## Run
 
--- folder-sorter/run.py
+++ folder-sorter/run.py
@@ -3,12 +3,27 @@ from os.path import isfile, join, isdir, join
 from shutil import move
 
 inpath = input("Please enter text path to be sorted: ")
+grouped = input("Group similar file types (y/n): ")[0]
+
+groupings = {
+    "documents": ["pdf", "txt", "csv"],
+    "executables": ["dmg", "pkg"],
+    "images": ["jpg", "jpeg", "png", "svg"]
+}
+
 files = [f for f in listdir(inpath) if isfile(join(inpath, f))]
 
 print("Found {} files to be sorted".format(len(files)))
 for filename in files:
     ext = filename.split(".")[1]
     targetpath = join(inpath, ext)
+
+    if grouped.lower() == "y":
+        for grouping_folder, grouping_exts in groupings.items():
+            if ext in grouping_exts:
+                targetpath = join(inpath, grouping_folder)
+                break
+
     if isdir(targetpath) == False:
         mkdir(targetpath)
 
